e6f94cb628d26287813a79a084825a37a0215dc7
TURGUT TURKMEN <e2036226@cenginek>
2018-12-02 00:33:05 +0300

    commit

--- b.py
+++ b.py
@@ -2,76 +2,31 @@ import socket
 import sys
 import time
 
-# s: - Interface 0: "10.10.1.1"
-# b  - Interface 1: "10.10.1.2"
-# b  - Interface 2: "10.10.2.1"
-# b  - Interface 6: "10.10.4.1"
-# r1 - Interface 3: "10.10.2.2"
-# r1 - Interface 4: "10.10.3.1"
-# r2 - Interface 7: "10.10.4.2"
-# r2 - Interface 8: "10.10.5.1"
-# d  - Interface 5: "10.10.3.2"
-# d  - Interface 9: "10.10.5.2"
-
-# Interface 0: "10.10.1.1"
-# Interface 1: "10.10.1.2"
-# Interface 2: "10.10.2.1"
-# Interface 3: "10.10.2.2"
-# Interface 4: "10.10.3.1"
-# Interface 5: "10.10.3.2"
-# Interface 6: "10.10.4.1"
-# Interface 7: "10.10.4.2"
-# Interface 8: "10.10.5.1"
-# Interface 9: "10.10.5.2"
-
-ips = ["10.10.1.1",
-	   "10.10.1.2",
-	   "10.10.2.1",
-	   "10.10.2.1",
-	   "10.10.3.1",
-	   "10.10.3.2",
-	   "10.10.4.1",
-	   "10.10.4.2",
-	   "10.10.5.1",
-	   "10.10.5.2"]
-
-# Listen TCP connection from s with Interface 1
-
-sockets = []
-def InitializeConnections():
-	host_s = ips[1]		# s ip
-	port_s = 5000		
-	sock_s = socket.socket()
-	sock_s.bind((host_s, sock_s))
-	sockets.append(sock_s)
-
-	hostb_i2 = ips[2]
-	portb_i2 = 8000
-	r1 = (ips[3], 8001)
-	sock_r1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock_r1.bind((hostb_i2, portb_i2))
-	sockets.append(sock_r1)	
-	
-
-def TCPconnection():
-	sockets[0].listen(1)
-	c, addr = sockets[0].accept()
-	print("Connection from: " + str(addr)) 
-	while True:
-		try:
-			data = c.recv(1024)
-			sockets[1].sendto(data, (ips[3], 8001))
-			if not data:
-				break
-			print("From connected user: " + str(data))
-		except:
-			continue
-	sockets[0].close()
-	sockets[1].close()
+# s  (interface-0): "10.10.1.1"
+# b  (interface-1): "10.10.1.2"
+# b  (interface-2): "10.10.2.1"
+# b  (interface-6): "10.10.4.1"
+# r1 (interface-3): "10.10.2.2"
+# r1 (interface-4): "10.10.3.1"
+# r2 (interface-7): "10.10.4.2"
+# r2 (interface-8): "10.10.5.1"
+# d  (interface-5): "10.10.3.2"
+# d  (interface-9): "10.10.5.2"
 
 def Main():
-	InitializeConnections()
-	TCPconnection()
+	host = "10.10.1.2"
+	port = 8000
+	s = socket.socket()
+	s.bind((host, port))
+	s.listen(1)
+	c, addr = s.accept()
+	for i in range(0, 100):
+		data = c.recv(1024)
+		if not data:
+			break
+		print "Received: " + data
+		ack = "ACK" + '{0:04}'.format(i)
+		c.send(ack)
 
 if __name__ == "__main__":
 	Main()
--- s.py
+++ s.py
@@ -2,50 +2,31 @@ import socket
 import sys
 import time
 
-# s: - Interface 0: "10.10.1.1"
-# b  - Interface 1: "10.10.1.2"
-# b  - Interface 2: "10.10.2.1"
-# b  - Interface 6: "10.10.4.1"
-# r1 - Interface 3: "10.10.2.2"
-# r1 - Interface 4: "10.10.3.1"
-# r2 - Interface 7: "10.10.4.2"
-# r2 - Interface 8: "10.10.5.1"
-# d  - Interface 5: "10.10.3.2"
-# d  - Interface 9: "10.10.5.2"
-
-# Interface 0: "10.10.1.1"
-# Interface 1: "10.10.1.2"
-# Interface 2: "10.10.2.1"
-# Interface 3: "10.10.2.2"
-# Interface 4: "10.10.3.1"
-# Interface 5: "10.10.3.2"
-# Interface 6: "10.10.4.1"
-# Interface 7: "10.10.4.2"
-# Interface 8: "10.10.5.1"
-# Interface 9: "10.10.5.2"
-
-ips = ["10.10.1.1",
-	   "10.10.1.2",
-	   "10.10.2.1",
-	   "10.10.2.1",
-	   "10.10.3.1",
-	   "10.10.3.2",
-	   "10.10.4.1",
-	   "10.10.4.2",
-	   "10.10.5.1",
-	   "10.10.5.2"]
+# s  (interface-0): "10.10.1.1"
+# b  (interface-1): "10.10.1.2"
+# b  (interface-2): "10.10.2.1"
+# b  (interface-6): "10.10.4.1"
+# r1 (interface-3): "10.10.2.2"
+# r1 (interface-4): "10.10.3.1"
+# r2 (interface-7): "10.10.4.2"
+# r2 (interface-8): "10.10.5.1"
+# d  (interface-5): "10.10.3.2"
+# d  (interface-9): "10.10.5.2"
 
 def Main():
-	# s with Interface 0 to b with Interface 1
-	host = ips[1]	# b's Interface 1 ip address
-	port = 5000		# The port used by b's Interface 1
+	host = "10.10.4.1" 		# B (inteface-6)
+	port = 8000
 
 	s = socket.socket()
 	s.connect((host, port))
-	for msg in range(100, 200):
-		s.send((str(msg) + "\n").encode())
-		print("PACKET->" + str(msg) + " sent", flush = True)
-		#time.sleep(0.1)
+
+	for i in range(0,100):
+		packetID = '{0:04}'.format(i)
+		message = "(PACKET:" + packetID ")" + "->s"
+		s.send(message.encode())
+		print message
+		ack = s.recv(1024)
+		print ack
 	s.close()
 
 if __name__ == "__main__":
