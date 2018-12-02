70b1f49ac49ddc0452e1b596c681d5b9de231601
TURGUT TURKMEN <e2036226@cenginek>
2018-12-02 16:50:04 +0300

    final

--- d.py
+++ d.py
@@ -2,6 +2,7 @@ import socket
 import time
 import threading
 
+e2e_delay = []
 def ListenR1():
 	host = "10.10.3.2"
 	port = 8000
@@ -14,9 +15,14 @@ def ListenR1():
 		rectime = time.time()
 		#t = int(time.time() * 1000)
 		e2e = int(rectime * 1000) - int(data[15:28])
+		e2e_delay.append(e2e)
 		print data + " with E2E: " + str(e2e)
 	 	ACK = "ACK(" + str(int(rectime * 1000)) + "):" + data[:13]
 		s.sendto(ACK, ("10.10.3.1", 8001))
+		if data[9:13] == "0999":
+	 		with open("e2e.csv", "wb") as csvfile:
+	 			patcher = csv.writer(csvfile, dialect='excel')
+	 			patcher.writerow(e2e_delay)
 	s.close()
 
 def ListenR2():
@@ -31,9 +37,14 @@ def ListenR2():
 	 	rectime = time.time()
 		#t = int(time.time() * 1000)
 		e2e = int(rectime * 1000) - int(data[15:28])
+		e2e_delay.append(e2e)
 		print data + " with E2E: " + str(e2e)
 	 	ACK = "ACK(" + str(int(rectime * 1000)) + "):" + data[:13]
 	 	s.sendto(ACK, ("10.10.5.1", 8001))
+	 	if data[9:13] == "0999":
+	 		with open("e2e.csv", "wb") as csvfile:
+	 			patcher = csv.writer(csvfile, dialect='excel')
+	 			patcher.writerow(e2e_delay)
 	s.close()
 
 def Main():
